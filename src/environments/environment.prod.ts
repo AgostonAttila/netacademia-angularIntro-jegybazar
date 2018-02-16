72205c8333d957e551f0d5c061921a5b39fda138
atesz014 <atesz014@gmail.com>
2018-02-16 18:56:37 +0100

    *some fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   firebase: {
-    baseUrl: 'https://jegybazar-66d0b.firebaseio.com',
+    baseUrl: '****',
     registrationUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
     loginUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword',
-    apikey: 'AIzaSyCHzqJu2b-jycYfrxsMB6EChQvjV9JfwAk'
+    apikey: '****'
   }
 };
